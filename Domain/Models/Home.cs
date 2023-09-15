afa66ee37a32c0e2798140d592614d702ff24dd3
MATHEUS DE CARVALHO SAMPAIO <matheus.sampaio.ms1@csn.com.br>
2023-09-15 14:50:58 -0300

    .

--- Domain/Models/Home.cs
+++ Domain/Models/Home.cs
@@ -1,10 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Domain.Models
 {
