f8ae0e91762ea462f995f9a18f30ec3d49bbc41d
MATHEUS DE CARVALHO SAMPAIO <matheus.sampaio.ms1@csn.com.br>
2023-09-18 12:33:08 -0300

    .

---
+++ Infra/Migrations/20230918143058_Initial_05.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infra.Migrations
+{
+    public partial class Initial_05 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Valor",
+                table: "TBL_Tarifas",
+                type: "VARCHAR(20)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Valor",
+                table: "TBL_Tarifas",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(20)");
+        }
+    }
+}
