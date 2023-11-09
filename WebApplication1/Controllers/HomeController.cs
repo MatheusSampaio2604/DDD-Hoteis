5d2c42c235735464743031ecb3c3868e0c332661
Matheus Sampaio <matheusdecarvalhosampaio@gmail.com>
2023-11-09 16:10:05 -0300

    api

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -2,9 +2,11 @@
 using Application.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 
@@ -22,16 +24,31 @@ namespace WebApi.Controllers
             _logger = logger;
             _IAcomodacaoApp = iAcomodacaoApp;
         }
-
-        [HttpGet("GetAll")]
+        [HttpGet("")]
         public async Task<IActionResult> Index()
         {
             var i = await _IAcomodacaoApp.FindAllAsync();
-            var iActive = i.Where(a => a.Ativo == true).ToList() ?? null;
-            if (iActive.Count > 0)
-                return Ok(true);
-            else
-                return BadRequest();
+            var iActive = i.Where(a => a.Ativo == true).ToList() ?? null;            
+
+            return Ok(iActive);
         }
+        [HttpGet("Privacidade")]
+        public IActionResult Privacy()
+        {
+            return Ok();
+        }
+
+        [HttpGet("Contato")]
+        public IActionResult Contact()
+        {
+            return Ok();
+        }
+
+        [HttpGet("PetFriendly")]
+        public IActionResult Pet()
+        {
+            return Ok();
+        }
+
     }
 }
